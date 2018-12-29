795bc63a24b9277326c3ef7f89936115bb930289
Darin Hunt <dhunt10@gmail.com>
2018-12-28 21:59:59 -0500

    done with 5

--- data_groups/project4/ex5.go
+++ data_groups/project4/ex5.go
@@ -4,9 +4,10 @@ import "fmt"
 
 func main() {
 	arr := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
-	fmt.Println(arr[:5])
-	fmt.Println(arr[5:])
-	fmt.Println(arr[2:7])
-	fmt.Println(arr[1:6])
+	
+	fmt.Println(arr)
 
+	arr = append(arr[:3],arr[6:]...)
+
+	fmt.Println(arr)
 }
